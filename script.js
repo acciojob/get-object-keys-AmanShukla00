138136ca7926b4f179e721a8d679f889bca019cb
AmanShukla00 <aman17101998@gmail.com>
2025-02-10 12:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+window.student = student;
+window.getKeys = getKeys;
